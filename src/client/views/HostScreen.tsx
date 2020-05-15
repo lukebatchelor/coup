cbebc2d2eda9274b51fe3409843532cde4739112
Luke.Batchelor <batchelor.luke+git@gmail.com>
2020-05-15 14:43:41 +1000

    Adds some host resolution ui

--- src/client/views/HostScreen.tsx
+++ src/client/views/HostScreen.tsx
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { makeStyles, Container, Paper, Typography, Grid, Avatar, Box, Button } from '@material-ui/core';
-import { State } from './PlayingScreen/types';
+import { State, getStateInfo } from './PlayingScreen/types';
 import { SocketContext, PlayerContext } from '../contexts';
+
+const NO_ACTION_TIMEOUT_MS = 10000;
+const RESOLUTION_PAUSE_MS = 3000;
 
 const useStyles = makeStyles((theme) => ({
   paper: { padding: theme.spacing(2) },
@@ -17,6 +20,69 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
   },
 }));
+
+function getLastActionText(playerAction: PlayerAction, state: GameState) {
+  const { lastAction, curTurnName } = getStateInfo(state);
+  if (state.actionStack.length === 0) {
+    return `${curTurnName}'s Turn`;
+  }
+  const { action, player } = playerAction;
+  const playerName = state.players[player].nickname;
+  const idxToName = (idx: number) => state.players[idx].nickname;
+
+  switch (action.type) {
+    case 'Income':
+      return `${playerName} is collecting income (+1 coin)`;
+    case 'Foreign Aid':
+      return `${playerName} is collecting foreign aid (+2 coins)`;
+    case 'Coup':
+      return `${playerName} is paying 7 coins  to stage a coup against ${idxToName(action.target)}`;
+    case 'Tax':
+      return `${playerName} is collecting tax as the Duke (+3 coins)`;
+    case 'Assassinate':
+      return `${playerName} is paying 3 coins to assasinate ${idxToName(action.target)}`;
+    case 'Exchange':
+      return `${playerName} is exchanging cards with the deck`;
+    case 'Steal':
+      return `${playerName} is stealing coins from ${idxToName(action.target)}`;
+    case 'Challenge':
+      return `${playerName} is challenging the action!`;
+    case 'Block':
+      return `${playerName} is blocking the action using a ${action.card}`;
+    case 'Exchanging Influence':
+      return `${playerName} is exchanging cards with the deck`;
+    case 'Revealing Influence':
+      return `${playerName} must reveal a card!`;
+    case 'Resolving':
+      return 'End of round...';
+    case 'Declare Winner':
+      return `${playerName} wins!`;
+  }
+}
+
+function getResolvedState(state: GameState, resolutionCount: number) {
+  if (state.actionStack.length !== 1 || state.actionStack[0].action.type !== 'Resolving') {
+    return state;
+  }
+  // create copy of state to modify
+  const resolvedState: GameState = JSON.parse(JSON.stringify(state));
+  const { resolutionActions } = state;
+  for (let i = 0; i < resolutionCount; i++) {
+    const action = resolutionActions[i];
+    if (action.type === 'Flip') {
+      resolvedState.hands[action.player].find((card) => card.card === action.card).flipped = true;
+    } else if (action.type === 'Discard') {
+    } else if (action.type === 'Draw') {
+    } else if (action.type === 'Gain Coins') {
+      resolvedState.players[action.gainingPlayer].coins += action.coins;
+    } else if (action.type === 'Lose Coins') {
+      resolvedState.players[action.losingPlayer].coins -= action.coins;
+    }
+  }
+
+  return resolvedState;
+}
+
 function noPlayerMoves(state: GameState) {
   return state.actions.every((actions) => {
     const { characterActions, bluffActions, generalActions, chooseActions } = actions;
@@ -29,15 +95,20 @@ function noPlayerMoves(state: GameState) {
     );
   });
 }
+
 type HostScreenProps = {};
 export function HostScreen(props: HostScreenProps) {
   const classes = useStyles();
   const socket = useContext(SocketContext);
   const [playerInfo, setPlayerInfo] = useContext(PlayerContext);
   const [state, setState] = useState<GameState>(null);
+  const [resolutionCount, setResolutionCount] = useState<number>(0);
 
   function resolveAction() {
-    socket.emit('resolve-action');
+    if (resolutionCount === state.resolutionActions.length) {
+      return socket.emit('resolve-action');
+    }
+    setResolutionCount(resolutionCount + 1);
   }
 
   useEffect(() => {
@@ -54,10 +125,24 @@ export function HostScreen(props: HostScreenProps) {
 
   if (!state) return <div>Loading...</div>;
 
+  const resolvedState = getResolvedState(state, resolutionCount);
+  const debugResolveText = `Resolution action: ${resolutionCount}/${state.resolutionActions.length}`;
   return (
     <Container maxWidth="lg">
+      {state.actionStack.length === 0 && (
+        <Typography variant="h5" gutterBottom align="center">
+          {`${getStateInfo(state).curTurnName}'s turn`}
+        </Typography>
+      )}
+      {state.actionStack.map((action) => (
+        <Typography variant="h5" gutterBottom align="center">
+          {getLastActionText(action, state)}
+        </Typography>
+      ))}
+
+      <Box mt={2} />
       <Grid container spacing={3} justify="center">
-        {state.players.map((player) => (
+        {resolvedState.players.map((player) => (
           <Grid item xs={3}>
             <Paper className={classes.paper}>
               <Box display="flex" flexDirection="row" alignItems="center" mb={2}>
@@ -70,7 +155,7 @@ export function HostScreen(props: HostScreenProps) {
                 </Box>
               </Box>
               <Grid container>
-                {state.hands[player.index].map((card) => {
+                {resolvedState.hands[player.index].map((card) => {
                   const cardUrl = card.flipped ? '/card.png' : '/card-back.png';
                   return (
                     <Grid item xs={6}>
@@ -90,7 +175,7 @@ export function HostScreen(props: HostScreenProps) {
         disabled={state.actionStack.length === 0}
         onClick={resolveAction}
       >
-        [DEBUG] Resolve Action
+        DEBUG_{debugResolveText}
       </Button>
     </Container>
   );
